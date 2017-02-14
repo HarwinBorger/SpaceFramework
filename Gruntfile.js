f9e2f2f1a1bdb8729ac073bf9946335f99f345f8
Harwin Borger <harwin@studioborger.nl>
2017-02-14 23:28:16 +0100

    Add first draft of space-model

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function (grunt)
+{
+
+	require('time-grunt')(grunt);
+
+	grunt.config.init({
+		docRoot: '',
+		dir: {
+			scss: 'scss',
+			dist: 'dist'
+		},
+		sass: {
+			style: {
+				files: [
+					{
+						expand: true,
+						cwd: "<%= dir.scss %>",
+						src: ["*.scss"],
+						dest: "<%= dir.dist %>/css",
+						ext: '.min.css'
+					}
+				],
+				options: {
+					cleancss: true,
+					outputStyle: 'compressed',
+					//sourceComments: 'map',
+					sourceMap: true
+				}
+			}
+		},
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					require('autoprefixer')({browsers: ['last 5 versions', 'ie >= 9']}),
+					require('csswring')
+				]
+			},
+			dist: {
+				src: "<%= dir.dist %>/css/**/*.css"
+			}
+		},
+		merge_media: {
+			options: {
+				compress: true
+			},
+			files: {
+				src: "<%= dir.assets %>/css/style.min.css",
+				dest: "<%= dir.assets %>/css/style.min.css"
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+
+			sass: {
+				files: ['<%= dir.scss %>/**/*.{scss,sass}'],
+				tasks: ['sass', 'postcss']
+			}
+		},
+		csscomb: {
+			options: {
+				config: '.csscomb.json'
+			},
+			dynamic_mappings: {
+				expand: true,
+				cwd: '<%= dir.assets %>/scss/',
+				src: ['**/*.scss'],
+				dest: '<%= dir.assets %>/scss/',
+				ext: '.scss'
+			}
+		}
+	});
+
+	grunt.registerTask( 'default', ['csscomb', 'sass',  'postcss', 'merge_media', 'watch'] );
+
+	grunt.registerTask('optimize', ['csscomb']);
+
+	grunt.registerTask('build', ['sass', 'postcss', 'merge_media']);
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+};
