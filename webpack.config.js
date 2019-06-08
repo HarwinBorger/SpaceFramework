90e8aa104e8b11a990becf19d7bc497c6775b166
Harwin Borger <harwin@studioborger.nl>
2019-06-08 15:19:11 +0200

    Make spaceframework default output name for css file

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			// Options similar to the same options in webpackOptions.output
 			// both options are optional
-			filename: "[name].css",
+			filename: "spaceframework.css",
 			chunkFilename: "[id].css"
 		})
 	]
