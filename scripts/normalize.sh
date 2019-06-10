3ae5d42a17bb4481cb9f9d1c4d60cff937ff9253
Harwin Borger <harwin@studioborger.nl>
2019-06-10 22:04:12 +0200

    add todo for normalize script

--- scripts/normalize.sh
+++ scripts/normalize.sh
@@ -1,4 +1,5 @@
 echo 'Update Normalize.css for SpaceFramework'
+#todo add npm update normalize code
 FILE=./node_modules/normalize.css/normalize.css
 CONTENT=`< $FILE tr '\n' '$'`
 sed "s~__NORMALIZE__~${CONTENT}~g" ./scripts/normalize.scss.template | tr '$' '\n'  > test.scss
