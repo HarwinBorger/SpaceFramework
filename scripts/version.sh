f2570a108694c2dd35028231821bd6457695a0d5
Harwin Borger <harwin@studioborger.nl>
2019-06-12 21:08:39 +0200

    Write given version to bower.json, package and composer.json

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,48 @@
-    #!/bin/bash
-    # Ask for the version
-    echo What is the new version?
-    read version
+#!/bin/bash
+# Colors
+HIGHLIGHT='\033[38;2;255;0;255m'
+NC='\033[0m' # No Color
 
-    composer config version $version
-    echo "Changed 'composer.json' version to: $version"
-#    npm version $version
-#    echo "Changed 'package.json' version to: $version"
+# Welcome
+echo "SpaceFramework version management"
+# Ask for the version
+echo -e "${HIGHLIGHT}Enter the new version:${NC}"
+read version
+
+# Set version npm (package.json)
+packageFiles=("composer.json" "package.json" "bower.json");
+
+# Loop through each setting manager file
+for packageFile in "${packageFiles[@]}"
+do
+	# Variables
+	temporaryFile=${packageFile}.tmp
+	backupFile=${packageFile}.backup
+
+	echo "---------------------------------"
+	echo "Updating $packageFile..."
+	# Create temporary file
+	node > $temporaryFile <<EOF
+	var data = require('./${packageFile}');
+	//Read data
+
+	//Manipulate data
+	data.version = '$version';
+
+	//Output data
+	console.log(JSON.stringify(data, null, 2));
+EOF
+
+	# Backup old file
+	mv -v $packageFile $backupFile
+	# Rename temporary file
+	mv -v $temporaryFile $packageFile
+	# Remove backup file
+	rm -v $backupFile
+
+	# Print update message
+	echo "Changed version in '$packageFile' to: $version"
+done
+
 
 
